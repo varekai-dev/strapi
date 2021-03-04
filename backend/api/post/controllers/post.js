1ee0e6a5aa54ddcf1ccfb8333923fef69d5d1688
varekaiwork <serhiisaviukwork@gmail.com>
2021-03-04 16:56:47 +0200

    add controller

--- backend/api/post/controllers/post.js
+++ backend/api/post/controllers/post.js
@@ -10,16 +10,20 @@ module.exports = {
     let entity;
     if (ctx.is("multipart")) {
       const { data, files } = parseMultipartData(ctx);
+      if (!data || !data.description) {
+        ctx.throw(400, "Please add some content");
+      }
+      if (!files || !files.length) {
+        ctx.throw(400, "Please add at least a file");
+      }
       entity = await strapi.services.post.create(
         { ...data, likes: 0 },
         { files }
       );
     } else {
-      entity = await strapi.services.post.create({
-        ...ctx.request.body,
-        likes: 0,
-      });
+      ctx.throw(400, "You must submit a multipart request");
     }
+
     return sanitizeEntity(entity, { model: strapi.models.post });
   },
 };
--- frontend/src/pages/Create.js
+++ frontend/src/pages/Create.js
@@ -3,28 +3,58 @@ import React, { useState } from 'react'
 function Create() {
 	const [description, setDescription] = useState('')
 	const [file, setFile] = useState(null)
+	const [error, setError] = useState('')
 	console.log(file)
 	const baseURI = 'http://localhost:1337'
 	const handleSubmit = async (e) => {
 		e.preventDefault()
 
+		if (!description) {
+			setError('Please add description')
+			return
+		}
+		if (!file) {
+			setError('Please add file')
+			return
+		}
+
 		const formData = new FormData()
 		formData.append('data', JSON.stringify({ description }))
 		formData.append('files.image', file)
-		const response = await fetch(`${baseURI}/posts`, {
-			method: 'POST',
+		try {
+			await fetch(`${baseURI}/posts`, {
+				method: 'POST',
 
-			body: formData
-		})
-		const data = await response.json()
-		console.log('data', data)
+				body: formData
+			})
+			// const data = await response.json()
+		} catch (err) {
+			setError(err)
+		}
 	}
 	return (
 		<div className="Create">
 			<h2>Create</h2>
+
+			{error && <p>{error}</p>}
 			<form onSubmit={handleSubmit}>
-				<input type="text" placeholder="Description" onChange={(e) => setDescription(e.target.value)} value={description} />
-				<input type="file" placeholder="Add file" onChange={(e) => setFile(e.target.files[0])} />
+				<input
+					type="text"
+					placeholder="Description"
+					onChange={(e) => {
+						setError('')
+						setDescription(e.target.value)
+					}}
+					value={description}
+				/>
+				<input
+					type="file"
+					placeholder="Add file"
+					onChange={(e) => {
+						setError('')
+						setFile(e.target.files[0])
+					}}
+				/>
 				<button>Submit</button>
 			</form>
 		</div>
