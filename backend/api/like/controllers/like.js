b2035f5ceb6b7728196216784c3527e10f38d23d
varekaiwork <serhiisaviukwork@gmail.com>
2021-03-09 15:07:56 +0200

    likes

--- backend/api/like/controllers/like.js
+++ backend/api/like/controllers/like.js
@@ -1,8 +1,68 @@
-'use strict';
+"use strict";
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    let entity;
+    const { user } = ctx.state; //Logged in user
+    const { post } = ctx.request.body; //id of Post
+    if (typeof post !== "number") {
+      ctx.throw(400, "Please only pass the id for the post");
+    }
+
+    const realPost = await strapi.services.post.findOne({ id: post });
+    if (!realPost) {
+      ctx.throw(400, "This post doesn't exist");
+    }
+    const found = await strapi.services.like.findOne({
+      user: user.id,
+      post,
+    });
+    if (found) {
+      ctx.throw(400, "You already liked this post");
+    }
+    if (ctx.is("multipart")) {
+      ctx.throw(400, "Only make JSON requests");
+    } else {
+      entity = await strapi.services.like.create({ post, user });
+    }
+    //Update the likes counter
+
+    const { likes } = realPost;
+    const updatePost = await strapi.services.post.update(
+      { id: post },
+      { likes: likes + 1 }
+    );
+    return sanitizeEntity(entity, { model: strapi.models.like });
+  },
+
+  async delete(ctx) {
+    const { user } = ctx.state;
+    const { postId } = ctx.params;
+
+    const post = parseInt(postId);
+    if (typeof post !== "number") {
+      ctx.throw(400, "Please use only id of post");
+    }
+
+    const entity = await strapi.services.like.delete({
+      post,
+      user: user.id,
+    });
+    if (entity.length) {
+      const { likes } = entity[0].post;
+      const updatePost = await strapi.services.post.update(
+        {
+          id: post,
+        },
+        { likes: likes - 1 }
+      );
+      return sanitizeEntity(entity[0], { model: strapi.models.like });
+    }
+  },
+};
--- frontend/src/pages/SinglePost.js
+++ frontend/src/pages/SinglePost.js
@@ -1,104 +1,98 @@
-import React, { useState, useEffect, useContext } from "react";
-import Post from "../components/Post";
+import React, { useState, useEffect, useContext } from 'react'
+import Post from '../components/Post'
 
-import { UserContext } from "../context/UserContext";
+import { UserContext } from '../context/UserContext'
 
 function SinglePost({ match, history }) {
-  const { user } = useContext(UserContext);
-  const { id } = match.params;
-  const [post, setPost] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
-  const [edit, setEdit] = useState(false);
-  //used for edit form
-  const [description, setDescription] = useState("");
+	const { user } = useContext(UserContext)
+	const { id } = match.params
+	const [post, setPost] = useState({})
+	const [loading, setLoading] = useState(true)
+	const [error, setError] = useState('')
+	const [edit, setEdit] = useState(false)
+	//used for edit form
+	const [description, setDescription] = useState('')
 
-  const fetchPost = async () => {
-    try {
-      const response = await fetch(`http://localhost:1337/posts/${id}`);
-      const data = await response.json();
-      setPost(data);
-      setLoading(false);
-      setDescription(data.description);
-    } catch (e) {
-      setLoading(false);
-      setError(e);
-    }
-  };
-  const handleEditSubmit = async (e) => {
-    e.preventDefault();
+	const fetchPost = async () => {
+		try {
+			const response = await fetch(`http://localhost:1337/posts/${id}`)
+			const data = await response.json()
+			setPost(data)
+			setLoading(false)
+			setDescription(data.description)
+		} catch (e) {
+			setLoading(false)
+			setError(e)
+		}
+	}
+	const handleEditSubmit = async (e) => {
+		e.preventDefault()
 
-    await fetch(`http://localhost:1337/posts/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${user.jwt}`,
-      },
-      body: JSON.stringify({
-        description,
-      }),
-    });
-    fetchPost();
-  };
+		await fetch(`http://localhost:1337/posts/${id}`, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${user.jwt}`
+			},
+			body: JSON.stringify({
+				description
+			})
+		})
+		fetchPost()
+	}
 
-  const handleDelete = async () => {
-    await fetch(`http://localhost:1337/posts/${id}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${user.jwt}`,
-      },
-    });
-    history.push("/");
-  };
-  useEffect(() => {
-    fetchPost();
-  }, []);
-  return (
-    <div className="Single__Post">
-      {!error ? (
-        <>
-          {loading && <p>Loading...</p>}
-          {!loading && (
-            <>
-              {post.id && (
-                <>
-                  <Post
-                    description={post.description}
-                    url={post.image && post.image.url}
-                    likes={post.likes}
-                  />
-                  {user && (
-                    <>
-                      <button onClick={handleDelete}>Delete this Post</button>
-                      <button onClick={() => setEdit(!edit)}>
-                        {!edit ? "Edit" : "Cancel"}
-                      </button>
-                    </>
-                  )}
-                  {edit && (
-                    <form onSubmit={handleEditSubmit}>
-                      <input
-                        type="text"
-                        value={description}
-                        onChange={(e) => {
-                          setDescription(e.target.value);
-                        }}
-                        placeholder="new description"
-                      />
-                      <button>Confirm</button>
-                    </form>
-                  )}
-                </>
-              )}
-              {!post.id && <p>404 - not found</p>}
-            </>
-          )}
-        </>
-      ) : (
-        <p>{error.message}</p>
-      )}
-    </div>
-  );
+	const handleDelete = async () => {
+		await fetch(`http://localhost:1337/posts/${id}`, {
+			method: 'DELETE',
+			headers: {
+				Authorization: `Bearer ${user.jwt}`
+			}
+		})
+		history.push('/')
+	}
+	useEffect(() => {
+		fetchPost()
+	}, []) //eslint-disable-line
+	return (
+		<div className="Single__Post">
+			{!error ? (
+				<>
+					{loading && <p>Loading...</p>}
+					{!loading && (
+						<>
+							{post.id && (
+								<>
+									<Post description={post.description} url={post.image && post.image.url} likes={post.likes} />
+									{user && (
+										<>
+											<button onClick={handleDelete}>Delete this Post</button>
+											<button onClick={() => setEdit(!edit)}>{!edit ? 'Edit' : 'Cancel'}</button>
+										</>
+									)}
+									{edit && (
+										<form onSubmit={handleEditSubmit}>
+											<input
+												type="text"
+												value={description}
+												onChange={(e) => {
+													setDescription(e.target.value)
+												}}
+												placeholder="new description"
+											/>
+											<button>Confirm</button>
+										</form>
+									)}
+								</>
+							)}
+							{!post.id && <p>404 - not found</p>}
+						</>
+					)}
+				</>
+			) : (
+				<p>{error.message}</p>
+			)}
+		</div>
+	)
 }
 
-export default SinglePost;
+export default SinglePost
